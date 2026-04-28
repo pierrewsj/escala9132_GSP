04188bd746f1afbf609773999248223d8afb30aa
pierrewsj <pierrewenderson1@gmail.com>
2026-04-28 18:07:13 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'escala-9132-v8-admin-simples';
+const FILES = ['./', './index.html', './style.css', './script.js', './dados.js', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request, { cache: 'no-store' })
+      .then(response => {
+        const clone = response.clone();
+        if (response && response.status === 200) caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
